722b434ee63da8abb7eeea19bacd0b7078ddc2dc
budhrajaashwin <budhrajaashwin@gmail.com>
2016-03-04 13:41:08 +0530

    model/about.js deleted

--- models/about.js
+++
@@ -1,8 +0,0 @@
-var keystone = require('keystone');
-var Types = keystone.Field.Types;
-
-var About = new keystone.List('About', {
-	autokey: { from: 'name', path: 'key', unique: true }
-});
-
-About.register();
