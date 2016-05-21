6ee67003c28a5f8e1548b69d01d47a399cd895b7
abhimediratta <abhimediratta@gmail.com>
2016-05-21 10:05:51 +0530

    more UI changes

--- public/js/displayProduct.js
+++ public/js/displayProduct.js
@@ -1,6 +1,6 @@
 $(".productImage").click(function(){
     $("#primaryImage").attr("src", $(this).attr("src"));
-    $('.productImage').css('border','0px solid #009440');
+    $('.productImage').css('border','1px solid #f2f2f2');
     $(this).css('border','1px solid #009440');
     // border: 1px solid #009440;
 });
